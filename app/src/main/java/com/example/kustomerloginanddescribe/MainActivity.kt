18f2f5ed18e73fe5b964aeb7c796cd08cf78c6be
Rachael Hobbs <hobbs.rachael@gmail.com>
2021-06-02 13:30:03 -0400

    set up ViewBinding

--- app/src/main/java/com/example/kustomerloginanddescribe/MainActivity.kt
+++ app/src/main/java/com/example/kustomerloginanddescribe/MainActivity.kt
@@ -2,16 +2,20 @@ package com.example.kustomerloginanddescribe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kustomerloginanddescribe.databinding.MainActivityBinding
 import com.example.kustomerloginanddescribe.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
+                .replace(R.id.fragment_container, MainFragment.newInstance())
                 .commitNow()
         }
     }
