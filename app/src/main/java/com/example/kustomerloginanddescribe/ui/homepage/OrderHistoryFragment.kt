74ec70562d5de1cc5c0a2c635ea130a5a0ecaa73
Rachael Hobbs <hobbs.rachael@gmail.com>
2021-06-03 18:02:02 -0400

    add fake orders with placeholder data

--- app/src/main/java/com/example/kustomerloginanddescribe/ui/homepage/OrderHistoryFragment.kt
+++ app/src/main/java/com/example/kustomerloginanddescribe/ui/homepage/OrderHistoryFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
+import com.example.kustomerloginanddescribe.R
 import com.example.kustomerloginanddescribe.databinding.FragmentOrderHistoryBinding
 
 class OrderHistoryFragment : Fragment() {
@@ -34,12 +35,31 @@ class OrderHistoryFragment : Fragment() {
         viewModel = ViewModelProvider(this, viewModelFactory)
             .get(OrderHistoryViewModel::class.java)
 
-        Log.d("backstack", parentFragmentManager.backStackEntryCount.toString())
-        Log.d("backstack", parentFragmentManager.getBackStackEntryAt(0).toString())
-
-
         binding.run {
             welcome.text = "Welcome, ${args.email}"
+
+            order1.title.text = "Order 1"
+            order1.description.text = "2 personal pan pizzas"
+            order1.image.setImageResource(R.drawable.ic_baseline_cake_24)
+            order1.root.setOnClickListener {
+                Log.d("Order 1", "clicked")
+            }
+
+            order2.title.text = "Order 2"
+            order2.description.text = "1 large cheese pizza"
+            order2.image.setImageResource(R.drawable.ic_baseline_cake_24)
+            order2.root.setOnClickListener {
+                Log.d("Order 2", "clicked")
+            }
+
+
+            order3.title.text = "Order 3"
+            order3.description.text = "1 medium cheese pizza, 1 Cesar Salad"
+            order3.image.setImageResource(R.drawable.ic_baseline_cake_24)
+            order3.root.setOnClickListener {
+                Log.d("Order 3", "clicked")
+            }
+
         }
     }
 }
